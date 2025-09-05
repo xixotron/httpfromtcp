b0759bea84f8b8754294d235f594aed68ecf3820
xixotron <xixotron89@gmail.com>
2025-09-06 01:20:13 +0200

    refactored read lines as a function

--- main.go
+++ main.go
@@ -16,31 +16,47 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't open file %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading file %s\n", inputFilePath)
 
-	line := ""
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil && errors.Is(err, io.EOF) {
-			break
-		} else if err != nil {
-			fmt.Printf("Error reading file: %v\n", err)
-			break
-		}
-
-		parts := strings.Split(string(b[:n]), "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", line+parts[i])
-			line = ""
-		}
-		line += parts[len(parts)-1]
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
+}
 
-	if line != "" {
-		fmt.Printf("read: %q\n", line)
-		line = ""
-	}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lineChan := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lineChan)
+
+		line := ""
+
+		for {
+			buff := make([]byte, 8)
+			n, err := f.Read(buff)
+			if err != nil {
+				if line != "" {
+					lineChan <- line
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				log.Printf("Error reading file: %v\n", err)
+				break
+			}
+
+			parts := strings.Split(string(buff[:n]), "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				lineChan <- line + parts[i]
+				line = ""
+			}
+			line += parts[len(parts)-1]
+
+		}
+	}()
+
+	return lineChan
 }
